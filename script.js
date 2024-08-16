47e22515ef0cd08e6ab43fefc8c6bb1eb6e92c0f
jkhoo <jollikhoo@gmail.com>
2024-08-16 14:27:48 -0500

    Coin Toss correction

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ let numTosses = 1;
 
 function tossCoin() {
     const random = Math.floor(Math.random() * 2);
-    const resultElement = document.getElementById("result");
+    const result = document.getElementById("result");
     const coinImage = document.getElementById("coin-image");
-    const countElement = document.getElementById("count");
+    const count = document.getElementById("count");
     const flipSound = document.getElementById("flip-sound");
 
     // Determine the result based on the random number
     if (random === 0) {
-        resultElement.textContent = "Heads";
+        result.textContent = "Heads";
         coinImage.src = "images/coin-heads.png";
         heads++;
     } else {
-        resultElement.textContent = "Tails";
+        result.textContent = "Tails";
         coinImage.src = "images/coin-tails.png";
         tails++;
     }
